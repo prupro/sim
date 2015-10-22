4b70c99e381bf2054aa918dc9331dc1c6618ea64
prupro <pruviola@gmail.com>
2015-10-22 14:13:02 +0530

    E3 cooperation probability DONE. is corrected one actually correct?

---
+++ analyticCoopE3.m
@@ -0,0 +1,29 @@
+rho3 = zeros(6,1);
+gamma = 3;
+fz = @(z) (gamma*(z.^(gamma-1)))./((1+z.^gamma).^2);
+eps1 = @(psi,lambda) 2*((cos(psi)).^2)./(pi*(lambda+4*((cos(psi)).^2)));
+
+psimax = @(z) -acos(z/2);
+psimin = @(z) acos(z/2);
+
+
+%eps2 = @(z,lambda) (acos(z/2).*(z.^2))./(pi*(lambda+z.^2));
+eps2 = @(z,lambda) (acos(z/2))./(pi*(lambda+1));
+Lambda = [1,1/2,1/4,1/8,1/16,1/32];
+for i = 1:6
+    lambda = Lambda(i);
+    t = @(z,psi)fz(z).*eps1(psi,lambda);
+    t1 = integral2(t,0,2,-pi/2,psimax);
+    t2 = integral2(t,0,2,psimin,pi/2);
+   % t3 = integral(@(z)eps2(z).*(2*acos(z/2)),0,2);
+    t3 = integral(@(z) eps2(z,lambda).*fz(z),0,2);
+    t4=integral(@(psi)eps1(psi,lambda),-pi/2,pi/2)*integral(fz,2,Inf);
+
+    rho3(i) =  t1+t2+t3+t4;
+end
+plot(1./Lambda,rho3,'r')
+
+
+
+
+
